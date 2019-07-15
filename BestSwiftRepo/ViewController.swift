3cc0512fb29be84e4d6eed8f208bc08ea88eb6e9
thiagomfaria <thiagomachadofaria@hotmail.com>
2019-07-15 19:01:56 -0300

    Initial Commit

---
+++ BestSwiftRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestSwiftRepo
+//
+//  Created by Thiago M Faria on 15/07/19.
+//  Copyright © 2019 Thiago M Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
