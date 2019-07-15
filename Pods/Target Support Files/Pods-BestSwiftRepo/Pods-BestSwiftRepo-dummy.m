d96a027917916e2f8a4417987ce07a9f270f801a
thiagomfaria <thiagomachadofaria@hotmail.com>
2019-07-15 19:28:39 -0300

    Instala Pods , alamofire

---
+++ Pods/Target Support Files/Pods-BestSwiftRepo/Pods-BestSwiftRepo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestSwiftRepo : NSObject
+@end
+@implementation PodsDummy_Pods_BestSwiftRepo
+@end
---
+++ Pods/Target Support Files/Pods-BestSwiftRepoTests/Pods-BestSwiftRepoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestSwiftRepoTests : NSObject
+@end
+@implementation PodsDummy_Pods_BestSwiftRepoTests
+@end
